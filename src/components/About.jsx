72b53c877f1a769279b6d8a42ed131c764f6ab3f
GaneshArumugam05 <ganesh050801@gmail.com>
2025-01-13 16:48:26 +0530

    new commit codes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,8 @@ function About() {
       </div>
       <div className="flex justify-center mt-8">
         <a
-          href="/RESUME_GANESH_A_react.pdf" // Correct path from the public folder
           download="Ganesh_Arumugam_CV.pdf"
+          href="/RESUME_GANESH_A_React.pdf"
           className="text-white font-bold rounded-md bg-blue-600 h-12 w-32 flex items-center justify-center hover:bg-blue-800"
         >
           Download CV
